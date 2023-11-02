bce5de90668848b9fdc7541c445cba7d1d8d77fa
Thomas Shih <55861476+ThomasShih@users.noreply.github.com>
2023-11-01 20:55:31 -0600

    Docker setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -7,3 +8,7 @@ app = FastAPI()
 @app.get("")
 async def helloworld():
     return "helloworld!"
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
